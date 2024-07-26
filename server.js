647095a69325dedcc2e52bc44dc2693d551b8813
kunalevicshukla <kunal.shukla2002@gmail.com>
2024-07-27 01:45:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const colors = require("colors");
+const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./db");
 //dot config
